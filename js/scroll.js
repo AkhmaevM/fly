6eb6464052f190dd84f6e768fdb02abe1dadb9d7
AkhmaevM <m.axmaev@yandex.ru>
2026-06-22 23:07:48 +0300

    Finished animations in header section and created another one in luxury section

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,5 @@
-// use a script tag or an external JS file
 document.addEventListener('DOMContentLoaded', event => {
 	gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-	// gsap code here!
 
 	ScrollSmoother.create({
 		smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
@@ -10,10 +8,10 @@ document.addEventListener('DOMContentLoaded', event => {
 	})
 
 	const fadeIn = '.fade-in'
+	const rightFadeIn = '.right-fade-in'
 	const zoomInFadeIn = '.zoom-in-fade-in'
-	const welcome = '.welcome'
-	const rightFadeIn = '.rightFadeIn'
-	const welcome__plane = '.welcome__plane'
+
+	/* Global */
 
 	gsap.set([fadeIn, rightFadeIn, zoomInFadeIn], {
 		force3D: true,
@@ -22,38 +20,44 @@ document.addEventListener('DOMContentLoaded', event => {
 	})
 
 	gsap.from(fadeIn, {
-		y: -20,
+		y: -25,
 		opacity: 0,
-		duration: 0.8,
-		stargger: 0.1,
-		ease: 'power2.easy'
-	})
-
-	gsap.from(rightFadeInfadeIn, {
-		x: 20,
-		opacity: 0,
-		duration: 0.8,
-		stargger: 0.1,
+		duration: 1,
+		stagger: 0.1,
 		ease: 'power2.ease'
 	})
+
+	gsap.from(rightFadeIn, {
+		x: 25,
+		opacity: 0,
+		duration: 1,
+		stagger: 0.1,
+		ease: 'power2.ease'
+	})
+
+	/* Welcome section */
+	const welcome = '.welcome'
+	const welcome__plane = '.welcome__plane'
 
 	gsap.from(zoomInFadeIn, {
 		scale: 0.8,
 		opacity: 0,
 		duration: 0.8,
 		ease: 'power2.out',
-		stagger: 0.1
+		stagger: 0.1,
+		immediateRender: false
 	})
 
 	gsap.from(welcome__plane, {
 		x: -140,
 		y: 50,
 		duration: 1.5,
-		ease: 'power2.ease'
+		ease: 'power3.ease',
+		immediateRender: false
 	})
 
 	const welcomeTl = gsap.timeline({
-		ScrollTrigger: {
+		scrollTrigger: {
 			trigger: welcome,
 			start: 'top top',
 			end: '+=120%',
@@ -62,16 +66,98 @@ document.addEventListener('DOMContentLoaded', event => {
 	})
 
 	welcomeTl
-		.to(zoomInFadeIn, {
-			scale: 0.8,
-			opacity: 0,
-			ease: 'none'
-		}, 0)
-		.to(welcome__plane, {
-			x: 140,
-			y: -50
-		}, 0)
+		.to(
+			zoomInFadeIn,
+			{
+				scale: 0.8,
+				opacity: 0,
+				ease: 'none'
+			},
+			0
+		)
+		.to(
+			welcome__plane,
+			{
+				x: 140,
+				y: -50
+			},
+			0
+		)
 
+	/* Mission section */
+	const mission = '.mission'
+
+	gsap.utils.toArray(`${mission} > div`).forEach((item, index) => {
+		gsap.fromTo(
+			item,
+			{ y: 25, scale: 0.7, opacity: 0 },
+			{
+				y: 0,
+				scale: 1,
+				opacity: 1,
+				ease: 'power2.out',
+				scrollTrigger: {
+					trigger: item,
+					start: 'top 95%',
+					end: 'top 45%',
+					scrub: true
+				}
+			}
+		)
+	})
+
+	/* Luxury */
+	const luxury = '#luxury'
+	const luxuryPlane = '.luxury-flights__image'
+
+	gsap.from(`${luxury} > .container`, {
+		scale: 0.9,
+		opacity: 0,
+		x: -100,
+		duration: 1,
+		ease: 'power2.ease'
+	})
+
+	gsap.from(luxuryPlane, {
+		scale: 0.9,
+		opacity: 0,
+		x: 150,
+		duration: 1.5,
+		ease: 'power3.ease'
+	})
+
+	gsap.fromTo(
+		`${luxury} > .container`,
+		{ scale: 0.9, opacity: 0, x: -100 },
+		{
+			scale: 1,
+			opacity: 1,
+			x: 0,
+			ease: 'none',
+			scrollTrigger: {
+				trigger: luxury,
+				start: 'top 80%',
+				end: '+=80%',
+				scrub: true
+			}
+		}
+	)
+
+	// Самолёт luxury
+	gsap.to(luxuryPlane, {
+		x: -300,
+		scale: 1.2,
+		ease: 'none',
+		scrollTrigger: {
+			trigger: luxuryPlane,
+			start: 'top 65%', // свой старт
+			end: '+=120%',
+			scrub: true,
+			immediateRender: false
+		}
+	})
+
+	// Ensure that the ScrollTrigger is refreshed after the content is loaded
 	window.addEventListener('load', () => {
 		ScrollTrigger.refresh()
 	})
