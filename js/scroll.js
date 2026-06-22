dc4a88a64ac3cc8acab300f1a9ed85b4760ac5df
Magomed Akhmaev <m.axmaev@yandex.ru>
2026-06-22 13:01:25 +0300

    Added base animations

--- js/scroll.js
+++ js/scroll.js
@@ -4,16 +4,35 @@ document.addEventListener('DOMContentLoaded', event => {
 	// gsap code here!
 
 	ScrollSmoother.create({
-		smooth: 0.8, // how long (in seconds) it takes to "catch up" to the native scroll position
-		effects: true, // looks for data-speed and data-lag attributes on elements
+		smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+		effects: false, // looks for data-speed and data-lag attributes on elements
 		smoothTouch: 0.1 // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
 	})
+
+    gsap.set(['.fade-in', '.zoom-in-fade-in'],{
+        force3D: true,
+        transformPerspective: 1000,
+        backfaceVisibility: 'hidden'
+    })
 
 	gsap.from('.fade-in', {
 		y: 20,
 		opacity: 0,
-		duration: 0.6,
+		duration: 0.8,
 		stargger: 0.1,
 		ease: 'power2.out'
 	})
+
+    gsap.from('.zoom-in-fade-in', {
+        scale:0.8,
+        opacity: 0,
+        duration: 0.4,
+        stagger:0.1,
+        ease: 'power2.out'
+    })
+})
+
+
+window.addEventListener('load', ()=>{
+    ScrollTrigger.refresh();
 })
